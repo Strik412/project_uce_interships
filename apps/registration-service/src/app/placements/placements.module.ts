2f1fa52c75d57470c1c2d8bebf799a350ec691cb
Strik412 <alex_217@hotmail.es>
2026-01-17 17:54:50 -0500

    feat: add CertificateIntegrationService to PlacementsModule providers

--- apps/registration-service/src/app/placements/placements.module.ts
+++ apps/registration-service/src/app/placements/placements.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlacementEntity } from '../database/entities/placement.entity';
 import { PlacementsController } from './placements.controller';
 import { PlacementsService } from './placements.service';
+import { CertificateIntegrationService } from '../services/certificate-integration.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PlacementEntity])],
   controllers: [PlacementsController],
-  providers: [PlacementsService],
+  providers: [PlacementsService, CertificateIntegrationService],
   exports: [PlacementsService],
 })
 export class PlacementsModule {}
+
