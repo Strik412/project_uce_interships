3f467e5edc695a0002454bc929ab34763cfea509
Strik412 <alex_217@hotmail.es>
2026-01-17 17:54:01 -0500

    feat: update TypeORM configuration to include Certificate entity and disable synchronization

--- apps/document-management-service/src/database/typeorm.config.ts
+++ apps/document-management-service/src/database/typeorm.config.ts
@@ -2,6 +2,7 @@ import { ConfigService } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Document } from '../app/documents/domain/document.entity';
 import { DocumentTemplate } from '../app/documents/domain/document-template.entity';
+import { Certificate } from '../app/certificates/certificate.entity';
 
 export const getTypeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => {
   return {
@@ -11,8 +12,8 @@ export const getTypeOrmConfig = (configService: ConfigService): TypeOrmModuleOpt
     username: configService.get<string>('DB_USERNAME', 'practicas_user'),
     password: configService.get<string>('DB_PASSWORD', 'practicas_password'),
     database: configService.get<string>('DB_NAME', 'practicas_db'),
-    entities: [Document, DocumentTemplate],
-    synchronize: true,
+    entities: [Document, DocumentTemplate, Certificate],
+    synchronize: false,
     logging: configService.get<boolean>('DB_LOGGING', true),
     dropSchema: false,
   };
