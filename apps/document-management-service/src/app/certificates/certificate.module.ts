7482ff1cf94a492c5d0f9beb317b9318804f1f34
Strik412 <alex_217@hotmail.es>
2026-01-18 17:14:46 -0500

    fix: add HttpModule to certificate module imports

--- apps/document-management-service/src/app/certificates/certificate.module.ts
+++ apps/document-management-service/src/app/certificates/certificate.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
 import { Certificate } from './certificate.entity';
 import { CertificateService } from './certificate.service';
 import { CertificateController } from './certificate.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Certificate])],
+  imports: [TypeOrmModule.forFeature([Certificate]), HttpModule],
   providers: [CertificateService],
   controllers: [CertificateController],
   exports: [CertificateService],
